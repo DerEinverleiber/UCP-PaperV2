24d8b3f8ab4ca68a1718bb7054cd5ceef1ee5ca7
moritzsoelderer <moritzsoelderer@gmail.com>
2026-03-17 14:52:46 +0100

    fix(cl_optimizer): minor implementation mistakes

--- cl_optimizer.py
+++ cl_optimizer.py
@@ -1,11 +1,13 @@
+from __future__ import annotations
+
 from typing import Callable
 
 import numpy as np
 import pandas as pd
 
 
-def geometric_temp_schedule(init_temp: int, end_temp: int, num_iterations: int) -> np.ndarray:
-    return np.geomspace(init_temp, end_temp, num=num_iterations)
+def geometric_temp_schedule(init_temp: int, end_temp: int, num_steps: int) -> np.ndarray:
+    return np.geomspace(init_temp, end_temp, num=num_steps)
 
 
 def bounds_to_neighborhood_function(bounds: list[tuple[int, int]]) -> Callable[[list[int]], list[int]]:
@@ -22,31 +24,65 @@ def bounds_to_neighborhood_function(bounds: list[tuple[int, int]]) -> Callable[[
     return neighborhood_function
 
 
-def simulated_annealing(num_bits: int, neighborhood_fun: Callable[[list[int]], list[int]], lookup_table: pd.DataFrame, temp_schedule: np.ndarray) -> tuple[list[int], float]:
+def simulated_annealing(
+        num_bits: int,
+        neighborhood_fun: Callable[[list[int]], list[int]],
+        lookup_table: pd.DataFrame,
+        temp_schedule: np.ndarray,
+        temp_iterations: int,
+        return_history: bool = False,
+) -> tuple[list[int], float] | tuple[list[int], float, list[tuple[list[int], float]]]:
     x = np.random.choice([0, 1], size=num_bits)
     loss = lookup_table.loc[lookup_table['candidate'] == str(x), 'loss'].iloc[0]
+    best_x = x
+    best_loss = loss
+
+    history = [(x, loss)]
 
     for temp in temp_schedule:
-        x_new = neighborhood_fun(x)
+        for _ in range(temp_iterations):
+            x_new = neighborhood_fun(x)
 
-        entry = lookup_table.loc[lookup_table['candidate'] == str(x_new)]
-        loss_new = entry.loc[:, 'loss'].iloc[0]
-        net_power_io_diff = entry.loc[:, 'abs. diff. net power IO'].iloc[0]
+            entry = lookup_table.loc[lookup_table['candidate'] == str(x_new)]
+            loss_new = entry.loc[:, 'loss'].iloc[0]
+            net_power_io_diff = entry.loc[:, 'abs. diff. net power IO'].iloc[0]
 
-        print(f'Candidate: {x_new}; Loss: {round(loss_new, 5)}; abs. diff. net power IO: {round(net_power_io_diff, 5)} --- Best Candidate {x_new}; Best loss: {loss_new}')
+            if loss_new < loss or np.random.rand() < np.exp(-(loss_new - loss) / temp):
+                x, loss = x_new, loss_new
+                if loss_new < best_loss:
+                    best_x, best_loss = x_new, loss_new
+            
+            history.append((x_new.copy(), loss_new))
+            print(f'Candidate: {x_new}; Loss: {round(loss_new, 5)}; abs. diff. net power IO: {round(net_power_io_diff, 5)} --- Best Candidate {best_x}; Best loss: {best_loss}')
 
-        if loss_new < loss or np.random.rand() > np.exp(- (loss_new - loss)/temp):
-            x = x_new
-            loss = loss_new
 
-    return x, loss
+    if return_history:
+        return best_x, best_loss, history
+    else:
+        return best_x, best_loss
 
 
 if __name__ == "__main__":
+    init_temp = 100
+    end_temp = 1
+    num_steps = 100
+    temp_iterations = 80
+
     lookup_table = pd.read_csv('data/brute_force/candidate_space_256_instances_2026-03-14_15-30-37.csv')
-    temp_schedule = geometric_temp_schedule(100, 1, 80)
+    temp_schedule = geometric_temp_schedule(init_temp, end_temp, num_steps)
     neighborhood_function = bounds_to_neighborhood_function(bounds=[(0, 4)] * 4)
 
-    best_candidate, loss = simulated_annealing(4, neighborhood_function, lookup_table, temp_schedule)
+    best_candidate, loss, history = simulated_annealing(
+        4,
+        neighborhood_function,
+        lookup_table,
+        temp_schedule,
+        temp_iterations,
+        return_history=True,
+    )
+    pd.DataFrame(history, columns=['candidate', 'loss']).to_csv(
+        f'data/simulated_annealing/path_init_{init_temp}_end_{end_temp}_steps_{num_steps}_iter_{temp_iterations}'
+        f'__256_instances_2026-03-14_15-30-37.csv',
+    )
     print("Best candidate:", best_candidate)
     print("Loss:", loss)
