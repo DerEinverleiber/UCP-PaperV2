0716b64b05aa4a5037d9ef3045eea56935c52f1a
moritzsoelderer <moritzsoelderer@gmail.com>
2026-04-07 23:59:34 +0200

    feat(metrics): introduce separate tts_p metric implementation

--- metrics.py
+++ metrics.py
@@ -6,17 +6,20 @@ def raar(expected_loss: float, loss_space: np.ndarray | pd.Series) -> float:
     minimum = loss_space.min()
     return (mean - expected_loss) / (mean - minimum)
 
-def min_tts(p_star: np.ndarray, l_p: np.ndarray = None, return_tts_p = False):
+def tts_p(p_star, l_p: np.ndarray = None) -> np.ndarray:
     l_p = l_p or np.ones(p_star.shape[0])
 
     assert p_star.shape == l_p.shape
 
     with np.errstate(divide='ignore', invalid='ignore'): # suppress divide by zero error - np.where filters this
-        tts_p = np.where(p_star > 0, l_p * np.ceil(np.log(0.01) / (np.log(1 - p_star))), np.inf)
+        return np.where(p_star > 0, l_p * np.ceil(np.log(0.01) / (np.log(1 - p_star))), np.inf)
 
-    p_min_0_based = tts_p.argmin()
+def min_tts(p_star: np.ndarray, l_p: np.ndarray = None, return_tts_p = False):
+    tts_p_ = tts_p(p_star, l_p)
+
+    p_min_0_based = tts_p_.argmin()
 
     if return_tts_p:
-        return p_min_0_based + 1, tts_p[p_min_0_based], tts_p
+        return p_min_0_based + 1, tts_p_[p_min_0_based], tts_p_
 
-    return p_min_0_based + 1, tts_p[p_min_0_based]
+    return p_min_0_based + 1, tts_p_[p_min_0_based]
