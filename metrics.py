9b9c9602ce67ea39cfb3028145f1b69ede766ef1
moritzsoelderer <moritzsoelderer@gmail.com>
2026-04-20 14:21:20 +0200

    feat(cl plots): experimental changes to cl plots and metrics

--- metrics.py
+++ metrics.py
@@ -7,19 +7,21 @@ def raar(expected_loss: float, loss_space: np.ndarray | pd.Series) -> float:
     return (mean - expected_loss) / (mean - minimum)
 
 def tts_p(p_star, l_p: np.ndarray = None) -> np.ndarray:
-    l_p = l_p or np.ones(p_star.shape[0])
+    l_p = l_p or np.ones(p_star.shape)
 
     assert p_star.shape == l_p.shape
 
     with np.errstate(divide='ignore', invalid='ignore'): # suppress divide by zero error - np.where filters this
         return np.where(p_star > 0, l_p * np.ceil(np.log(0.01) / (np.log(1 - p_star))), np.inf)
 
-def min_tts(p_star: np.ndarray, l_p: np.ndarray = None, return_tts_p = False):
+def min_tts(p_star: np.ndarray, l_p: np.ndarray = None, return_tts_p = False, axis=-1):
     tts_p_ = tts_p(p_star, l_p)
 
-    p_min_0_based = tts_p_.argmin()
+    p_min_0_based = tts_p_.argmin(axis=axis)
+    indices_expanded = np.expand_dims(p_min_0_based, axis=-1)
+    min_values = np.take_along_axis(tts_p_, indices_expanded, axis=-1) .squeeze(axis=-1)
 
     if return_tts_p:
-        return p_min_0_based + 1, tts_p_[p_min_0_based], tts_p_
+        return p_min_0_based + 1, min_values, tts_p_
 
-    return p_min_0_based + 1, tts_p_[p_min_0_based]
+    return min_values
